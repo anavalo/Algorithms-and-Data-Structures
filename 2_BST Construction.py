e6d0494f64cf056ce5d32e086e01220e0736dd24
anavalo <tsournos.anastasios@gmail.com>
2019-06-05 12:37:54 +0200

    .

---
+++ 2_BST Construction.py
@@ -0,0 +1,29 @@
+'''
+Write a Binary Search Tree(BST) class. The class should have a 'value' property set to be an integer, as well as '"left"
+and "right" properties, both of which should point to either the None (null) value or to another BST. A node is said
+to be a BST node if and only if it satisfies the BST property: its value its strictly greater than the values of every
+node to its left; its value is less or equal to the values of every node to its right; and both of its children nodes
+are either BST nodes themselves or None(null) values. The BST class should support insertion, searching, and removal pf
+values. The removal method should only remove the first instance of the target value.
+'''
+
+class BST:
+
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def insert(selfself, value):
+
+
+        return self
+
+    def contains(self, value):
+
+
+
+    def remove(selfs, value):
+
+        return self
+
