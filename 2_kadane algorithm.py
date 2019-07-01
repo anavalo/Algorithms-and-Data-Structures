6961ece5b5a112605efefa9bb899a898747c3493
anavalo <tsournos.anastasios@gmail.com>
2019-07-01 15:14:39 +0200

    .

--- 2_kadane algorithm.py
+++ 2_kadane algorithm.py
@@ -1,25 +1,32 @@
 
 def kadanesAlgorithm(array):
-    lidx = int(len(array)/2)
-    ridx = int((len(array)/2)+1)
-    while True:
-        currentsum = (sum(array[lidx:ridx]))
-        maxsum = sum(array)
+    lidx = 0
+    ridx = (len(array))
+    sum_right = []
+    sum_left = []
+    x = sum(array)
+    z = max(array)
+    while lidx < ridx:
+        left_sum = sum(array[lidx:ridx])
+        sum_left.append(left_sum)
+        lidx +=1
+    for i, j in enumerate(sum_left):
+        if j == max(sum_left):
+            maxlidx = i
+    while ridx > 0:
+        right_sum = sum(array[0:ridx])
+        sum_right.append(right_sum)
+        ridx -= 1
+    for i, j in enumerate(sum_right):
+        if j == max(sum_right):
+            maxridx = i
+    final_Array = array[maxlidx:(-abs(maxridx))]
+    y =  sum(final_Array)
+    print(final_Array)
+    print(x, z, y)
+    return max(x, z, y)
 
 
+ex1 = [-1, -2, -3 , -4, -5, -6, -7, -8, -9, -10]
 
-
-
-
-
-
-
-
-
-
-
-
-
-ex1 = [3, 5, -9, 1, 3, -2, 3, 4, 7, 2, -9, 6, 3, 1, -5, 4]
-
-kadanesAlgorithm(ex1)
+print(kadanesAlgorithm(ex1))
