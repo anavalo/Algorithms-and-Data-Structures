9f501ee30ded62a7f3e426b39db8877a1d4c6dca
anavalo <tsournos.anastasios@gmail.com>
2019-07-03 18:37:53 +0200

    .

--- 2_Maximum Subset Sum With No Adjacent Elements.py
+++ 2_Maximum Subset Sum With No Adjacent Elements.py
@@ -1,9 +1,18 @@
+'''Write a function that takes in an array of positive integers and returns an integer representing the maximum sum of
+nob-adjacent elements in the array. Ia a sum cannot be generated, the function should return 0'''
+
 def maxSubsetSumNoAdjacent(array):
-
-
-
-
-
+    # edge case of empty array
+    if not len(array):
+        return 0
+    # edge case whre lenght of array = 1
+    elif len(array) == 1:
+        return array[0]
+    maxSums = array
+    maxSums[1] = max(array[0], array[1])
+    for i in range (2, len(array)):
+        maxSums[i] = max(maxSums[i-1], maxSums[i-2] + array[i])
+    return maxSums[-1]
 
 
 
@@ -11,4 +20,7 @@ def maxSubsetSumNoAdjacent(array):
 
 mmm = [7, 10, 12, 7, 9, 14]
 
-maxSubsetSumNoAdjacent(mmm)
+print(maxSubsetSumNoAdjacent(mmm))
+
+
+
