77594c882c077e308ff207d1773f8bf7698bf609
anavalo <tsournos.anastasios@gmail.com>
2019-07-07 23:09:39 +0200

    .

--- Permutations.py
+++ Permutations.py
@@ -1,27 +1,32 @@
 '''write a function that takes in an array of unique integers and returns an array of all permutations of those
 integers. If the input is empty, the function should return an empty array'''
 
-def getPermutations(array):
-    permutations = []
-    permutationsHelper(array, [], permutations)
-    return permutations
+# def getPermutations(array):
+#     permutations = []
+#     permutationsHelper(array, [], permutations)
+#     return permutations
 
 def permutationsHelper(array, currentPermutation, permutations):
     if not len(array) and len(currentPermutation):
         permutations.append(currentPermutation)
     else:
         for i in range(len(array)):
+            print('i is: ' + str(i))
+
             newArray = array[:i] + array[i + 1:]
+            print('newArray is: ' + str(newArray))
+
             newPermutation = currentPermutation + [array[i]]
+            print('newPermutations is: ' + str(newPermutation) + '\n')
+
             permutationsHelper(newArray, newPermutation, permutations)
 
 
 
 mmm = [1,2,3]
+permutations = []
 
-
-
-print(mmm[1:])
+print(permutationsHelper(mmm, [], permutations))
 
 
 
