7935ebbcb40635f3bb39003b5cca94c570590bdc
anavalo <tsournos.anastasios@gmail.com>
2019-08-12 09:38:25 +0200

    .

---
+++ Longest Substring Without Duplication.py
@@ -0,0 +1,12 @@
+'''
+Write a function that takes in a string and that returns its longest substring without duplicate characters.
+Assume that there will only be one longest substring without duplication.
+Sample input: "clementisacap"
+Sample output: "mentisac"
+'''
+
+
+
+def longestSubString(string):
+
+
