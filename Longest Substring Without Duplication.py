4b169e40e8f5010d2adb9cfb109d86fcf0531301
anavalo <tsournos.anastasios@gmail.com>
2019-08-12 11:40:20 +0200

    .

--- Longest Substring Without Duplication.py
+++ Longest Substring Without Duplication.py
@@ -6,7 +6,27 @@ Sample output: "mentisac"
 '''
 
 
-
 def longestSubString(string):
+    lastSeen = {}
+    longest = [0,1]
+    startIdx = 0
+    for i, char in enumerate(string):
+        if char in lastSeen:
+            startIdx = max(startIdx, lastSeen[char])
+        if longest[1] - longest[0] < i + 1 - startIdx:
+            longest = [startIdx, i+1]
+        lastSeen[char] = i
+    return string[longest[0]:longest[1]]
 
+print(longestSubString('melenepopisantigiagiamouthnkalliopi'))
+
+
+
+
+
+
+
+
+
+longestSubString("clementisacap")
 
