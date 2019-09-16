f91ed6615f68007440c722be934b8916a50ec40c
anavalo <tsournos.anastasios@gmail.com>
2019-09-16 19:52:13 +0200

    .

--- Matrix in Spiral Form.py
+++ Matrix in Spiral Form.py
@@ -1,18 +1,35 @@
+import numpy
 def spiral(array):
-
     # k: start row idx
     # l: end row idx
     # m: start column idx
     # n: end column idx
-
-    k, m = 0, 0
-    n = len(array[0])-1
-    l = len(array)-1
     result = []
-    for i in range(m, n-1):
-        result.append([i][k])
-    return result
-
+    if len(array) != 0:
+        if len(array[0]) == 1:
+             result.append(array[0][0])
+             result.append(array[1][0])
+        else:
+            k, m = 0, 0
+            n = len(array[0]) - 1
+            l = len(array) - 1
+            while m <= n:
+                result.append(array[k][m])
+                m +=1
+            while k < l:
+                result.append(array[k+1][n])
+                k += 1
+            k, m = 0, 0
+            while n > m:
+                result.append(array[l][n-1])
+                n -=1
+            while l > k:
+                result.append(array[l-1][m])
+                l -= 1
+    newar = numpy.array(array)[1:-1, 1:-1]
+    x = newar.tolist()
+    print(result)
+    return result[:-1] + spiral(x)
 
 matrix = [[1, 2, 3],
           [4, 5, 6],
