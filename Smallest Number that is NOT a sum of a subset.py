6337b17dca6598ce322247e40af27fcc8bdefa25
anavalo <tsournos.anastasios@gmail.com>
2019-10-02 10:26:11 +0200

    .

--- Smallest Number that is NOT a sum of a subset.py
+++ Smallest Number that is NOT a sum of a subset.py
@@ -1,21 +1,29 @@
 '''Given a sorted list of positive numbers, find the smallest positive number that cannot be a sum of any subset in the list.'''
 
-def findSmallest(array):
+
+def findSmallest2(array):
+    missing = []
     hash = {}
-    candidates = []
     for num in array:
         hash[num] = True
-    for num in reversed(array):
-        number = num-1
-        while number in hash:
-            number -= 1
-        candidates.append(number)
-    print(candidates)
+    for i in range(1, max(array)):
+        if i not in hash:
+            missing.append(i)
+    for num in missing:
+        if not isSum(array, num):
+            return num
+
+
+def isSum(array, target):
+    if target == 0:
+        return True
+    if not len(array):
+        return False
+    if array[len(array)-1] > target:
+        return isSum(array[:-1], target)
+    return isSum(array[:-1], target-array[len(array)-1])
 
 
 
 
-
-
-
-print(findSmallest([1, 2, 8, 9, 10]))
+print(findSmallest2([1, 2, 3, 8, 9, 10]))
